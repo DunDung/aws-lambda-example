d476c553170b494b699c79f297af6df52d0263c5
dundung <ebseud6135@naver.com>
2022-04-19 17:05:10 +0900

    init project

---
+++ src/main/java/com/example/awslambdaexample/AwsLambdaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.awslambdaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsLambdaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awslambdaexample/AwsLambdaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awslambdaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLambdaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
